6c7abb5f504e6891fd0d8654d90916f8fc10bc67
eliot <eliot.galaup@epfedu.fr>
2026-03-18 16:47:02 +0100

    add assets

---
+++ src/assets/pont_half.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="pont_half" tilewidth="16" tileheight="16" tilecount="2688" columns="64">
+ <image source="../../../../../Downloads/pont_half.png" width="1024" height="682"/>
+</tileset>
